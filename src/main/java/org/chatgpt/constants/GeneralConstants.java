1e559667b698508c29e3c74eaa42ed6ea352c7f2
Влад Яницкий <vlad.comp.com@gmail.com>
2023-04-14 14:38:19 +0300

    Update git ignore

--- src/main/java/org/chatgpt/constants/GeneralConstants.java
+++
@@ -1,7 +0,0 @@
-package org.chatgpt.constants;
-
-public class GeneralConstants {
-
-    public final static String BOT_NAME = "bot.name";
-    public final static String BOT_SECRET = "bot.secret";
-}
