59a75fbb2700ef1361acf0115de92d9731fc2b9a
Влад Яницкий <vlad.comp.com@gmail.com>
2023-04-18 23:32:09 +0300

    Adds new image strategy - fix 11

--- src/main/java/org/chatgpt/api/dream/DreamApi.java
+++ src/main/java/org/chatgpt/api/dream/DreamApi.java
@@ -6,6 +6,7 @@ import com.google.gson.JsonObject;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpPut;
 import org.apache.http.entity.StringEntity;
@@ -29,6 +30,8 @@ public class DreamApi {
     private HttpClient client;
     private HttpPut put;
     private HttpPost post;
+    private HttpGet get;
+    private String taskId;
 
     public DreamApi() {
         this.dreamImage = new DreamImage();
@@ -39,6 +42,7 @@ public class DreamApi {
         this.client = HttpClientBuilder.create().build();
         this.put = new HttpPut();
         this.post = new HttpPost();
+        this.get = new HttpGet();
 
         put.setHeader("Authorization", "Bearer " + API_KEY);
         put.setHeader("Accept", "application/json");
@@ -47,6 +51,10 @@ public class DreamApi {
         post.setHeader("Authorization", "Bearer " + API_KEY);
         post.setHeader("Accept", "application/json");
         post.setHeader("Content-Type", "application/json; utf-8");
+
+        get.setHeader("Authorization", "Bearer " + API_KEY);
+        get.setHeader("Accept", "application/json");
+        get.setHeader("Content-Type", "application/json; utf-8");
     }
 
     private String createRequest(HttpEntityEnclosingRequestBase httpMethod) {
@@ -68,29 +76,42 @@ public class DreamApi {
 
     public InputFile generateImages(String styleId, String description) {
         createConnection();
+        createTaskId();
         StringEntity entity = new StringEntity(dreamImage.createRequest(styleId, description), StandardCharsets.UTF_8);
-        put.setURI(URI.create(URL + createTaskId()));
+        put.setURI(URI.create(URL + taskId));
         put.setEntity(entity);
         String requestResult = createRequest(put);
+        while (true) {
+            JsonObject result = checkGenerator();
+            System.out.println("Result in cycle: " + result);
+            JsonElement status = result.get("state");
+            if (status.getAsString().equals("pending")) {
+                System.out.println("Status: pending");
+            }
+            if (status.getAsString().equals("completed")) {
+                System.out.println("Status: completed");
+                break;
+            }
+            if (status.getAsString().equals("failed")) {
+                System.out.println("Status: failed");
+                break;
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+        }
         return dreamImage.createResponse(requestResult);
     }
 
-    private String createTaskId() {
-        String result = "";
+    private void createTaskId() {
         try {
             String jsonInputString = "{ \"use_target_image\": true }";
             StringEntity entity = new StringEntity(jsonInputString, StandardCharsets.UTF_8);
             post.setURI(URI.create(URL));
             post.setEntity(entity);
             String requestResult = createRequest(post);
-
-            System.out.println("Task Result: " + requestResult);
-
-//            String targetImageUrl = requestResult.split("\"target_image_url\":")[1];
-
-//            System.out.println("Task URL: " + targetImageUrl);
-
-//            result = targetImageUrl.split("/")[2];
 
             Gson jsonResult = new Gson().newBuilder().setPrettyPrinting().create();
             JsonObject object = jsonResult.fromJson(requestResult, JsonObject.class);
@@ -99,12 +120,37 @@ public class DreamApi {
 
             JsonElement id = object.get("id");
 
-            result = id.getAsString();
-            System.out.println("Task ID: " + result);
+            taskId = id.getAsString();
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        return result;
+    }
+
+    private JsonObject checkGenerator() {
+        JsonObject jsonObject = new JsonObject();
+        try {
+            get.setURI(URI.create(URL + taskId));
+
+            StringBuilder result = new StringBuilder();
+            try {
+                HttpResponse response = client.execute(get);
+                BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent(), StandardCharsets.UTF_8));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    result.append(line);
+                }
+            } catch (Exception e) {
+                System.out.println("Exception: " + e.getMessage());
+            }
+
+            Gson jsonResult = new Gson().newBuilder().setPrettyPrinting().create();
+            jsonObject = jsonResult.fromJson(result.toString(), JsonObject.class);
+
+            System.out.println("Response: " + jsonObject.toString());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return jsonObject;
     }
 
     public Map<String, String> getStyles() {
