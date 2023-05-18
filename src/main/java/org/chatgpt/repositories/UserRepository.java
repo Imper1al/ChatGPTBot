a0dea801c62282f067a4b01a73771aacb6e0e850
Влад Яницкий <vlad.comp.com@gmail.com>
2023-05-18 22:43:27 +0300

    Test Chat GPT 2.0 - database test 2

--- src/main/java/org/chatgpt/repositories/UserRepository.java
+++ src/main/java/org/chatgpt/repositories/UserRepository.java
@@ -2,9 +2,9 @@ package org.chatgpt.repositories;
 
 import org.chatgpt.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
-@Component
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User selectUserByChatId(String chatId);
