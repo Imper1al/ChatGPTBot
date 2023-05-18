fc9c2921fee834f9d1d6dca707c21c4aef097b71
Влад Яницкий <vlad.comp.com@gmail.com>
2023-05-19 00:02:50 +0300

    Test Chat GPT 2.0 - database test 19

--- src/main/java/org/chatgpt/entities/User.java
+++ src/main/java/org/chatgpt/entities/User.java
@@ -16,7 +16,7 @@ import javax.persistence.*;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
     @Column(name = "first_name")
     private String firstName;
