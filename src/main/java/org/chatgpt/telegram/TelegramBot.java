13f346efcebe741674cd8917ccc533e4dee025b9
Влад Яницкий <vlad.comp.com@gmail.com>
2023-05-19 13:55:02 +0300

    Chat GPT 2.0 - test 10

--- src/main/java/org/chatgpt/telegram/TelegramBot.java
+++ src/main/java/org/chatgpt/telegram/TelegramBot.java
@@ -50,7 +50,7 @@ public class TelegramBot extends TelegramLongPollingBot {
     private boolean isCreateAd = false;
     private boolean isCreateAdImage = false;
     private boolean isCreateAdMessage = false;
-    private final boolean tehrab = true;
+    private final boolean tehrab = false;
     private boolean isAdmin = false;
     private String currentAdminStrategy;
     private String quantity;
@@ -212,6 +212,7 @@ public class TelegramBot extends TelegramLongPollingBot {
             isHeight = false;
             isWidth = false;
             resetValues();
+            resetAdminValues();
         }
     }
 
